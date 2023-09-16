d503b195502cef9a881eafc777f765789a1dcbbb
Bhoomika <bhoomikahm18@gmail.com>
2023-09-16 20:39:16 +0530

    Adding Cross element

--- script.js
+++ script.js
@@ -6,10 +6,20 @@ const startCells = [
 ]
 
 function createBoard() {
-    startCells.forEach((cell, index) => {
+    startCells.forEach((_cell, index) => {
         const cellElement = document.createElement('div');
         cellElement.classList.add('square');
+        cellElement.id = index
+        cellElement.addEventListener('click', addGo)
         gameBoard.append(cellElement)
     })
 }
 createBoard();
+
+function addGo(e) {
+    // console.log(e);
+    const goDisplay = document.createElement('div');
+    goDisplay.classList.add('cross');
+    e.target.append(goDisplay);
+}
+
