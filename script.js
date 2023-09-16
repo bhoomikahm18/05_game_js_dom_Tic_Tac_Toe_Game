5f0248aaaa5443ecc9f00723309d8d7fbb601a43
Bhoomika <bhoomikahm18@gmail.com>
2023-09-16 22:22:47 +0530

    Adding event listener to print circle and cross

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-const gameBoard = document.querySelector('#gameboard');
-const infoDisplay = document.querySelector("#info");
+const gameBoard = document.querySelector('#gameboard')
+const infoDisplay = document.querySelector("#info")
 
 const startCells = [
     "","","","","","","","",""
 ]
+let go  = "circle"
+infoDisplay.textContent = "Circle goes first"
 
 function createBoard() {
     startCells.forEach((_cell, index) => {
-        const cellElement = document.createElement('div');
-        cellElement.classList.add('square');
+        const cellElement = document.createElement('div')
+        cellElement.classList.add('square')
         cellElement.id = index
         cellElement.addEventListener('click', addGo)
         gameBoard.append(cellElement)
@@ -17,9 +19,12 @@ function createBoard() {
 createBoard();
 
 function addGo(e) {
-    // console.log(e);
-    const goDisplay = document.createElement('div');
-    goDisplay.classList.add('cross');
-    e.target.append(goDisplay);
+    // console.log(e.target);
+    const goDisplay = document.createElement('div')
+    goDisplay.classList.add(go)
+    e.target.append(goDisplay)
+    go = go === "circle" ? "cross" : "circle"
+    infoDisplay.textContent = "It is now " + go + "'s go."
+    e.target.removeEventListener("click", addGo);
 }
 
