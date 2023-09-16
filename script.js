99719e75f1c820e12df124ac2aeda21693242c0d
Bhoomika <bhoomikahm18@gmail.com>
2023-09-16 20:11:44 +0530

    Calling function after styling

--- script.js
+++ script.js
@@ -9,5 +9,7 @@ function createBoard() {
     startCells.forEach((cell, index) => {
         const cellElement = document.createElement('div');
         cellElement.classList.add('square');
+        gameBoard.append(cellElement)
     })
 }
+createBoard();
